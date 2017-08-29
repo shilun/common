050d79b6a052e3305723f7edd6e33d9a63d23ff7
Administrator <martin.shi@nidone.com.cn>
2017-08-29 11:49:20 +0800

    springboot

---
+++ src/main/java/com/common/webservice/ServerDataProvider.java
@@ -0,0 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.common.webservice;
+
+import java.util.Date;
+
+public interface ServerDataProvider {
+    Date buildServerData();
+}
