270e765a0e759fa549ff273134f5c6b48ee80a4f
shilun <martin.shi@nidone.com.cn>
2018-01-15 10:43:31 +0800

    springboot

--- src/main/java/com/common/web/interceptor/rpc/ResourcesRPCService.java
+++ src/main/java/com/common/web/interceptor/rpc/ResourcesRPCService.java
@@ -6,17 +6,21 @@
 package com.common.web.interceptor.rpc;
 
 import com.common.util.Result;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import java.util.List;
 
-@WebService
+@FeignClient("erp.rpc")
 public interface ResourcesRPCService {
     /**
      * 查找运维权限资源
      * @param pin
      * @return
      */
-    public Result<List<String>> findResources(@WebParam(name = "account") String pin);
+    @RequestMapping("/rpc/resource/findResources")
+    public Result<List<String>> findResources(@RequestParam(name = "pin") String pin);
 }
