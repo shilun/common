dbf43ca57bc13513a08e973b0122184cc1899288
lunsh <lunshi@tt.com>
2018-11-29 12:10:33 +0800

    修改mongodb sort

--- src/main/java/com/common/redis/DistributedLock.java
+++ src/main/java/com/common/redis/DistributedLock.java
@@ -3,7 +3,7 @@ package com.common.redis;
 public interface DistributedLock {
 
     /**
-     * 获取锁
+     * 获取锁 成功返回 true  失败返回false
      * @author yangwenkui
      * @time 2016年5月6日 上午11:02:54
      * @return
