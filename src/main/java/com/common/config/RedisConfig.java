2a0ed4b87da7bd6ae343426bf7c6d3e4fd0274ae
lunsh <lunshi@tt.com>
2018-11-29 20:16:19 +0800

    修改mongodb sort

--- src/main/java/com/common/config/RedisConfig.java
+++ src/main/java/com/common/config/RedisConfig.java
@@ -4,31 +4,14 @@ import com.common.redis.DistributedLockUtil;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.jdbc.pool.DataSourceProxy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.HostAndPort;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisCluster;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * redis
