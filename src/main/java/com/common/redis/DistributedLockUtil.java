3c4fb53dd1916fd5e27d971070580957f8f7a338
lunsh <lunshi@tt.com>
2018-12-01 18:56:47 +0800

    修改mongodb sort

--- src/main/java/com/common/redis/DistributedLockUtil.java
+++ src/main/java/com/common/redis/DistributedLockUtil.java
@@ -2,26 +2,27 @@ package com.common.redis;
 
 import org.redisson.api.RedissonClient;
 
-import javax.annotation.Resource;
-
-public class DistributedLockUtil{
-    @Resource
-     private RedissonClient redissonClient;
+public class DistributedLockUtil {
+    RedissonClient redissonClient;
     /**
      * 获取分布式锁
      * 默认获取锁10s超时，锁过期时间60s
+     *
+     * @return
      * @author yangwenkui
      * @time 2016年5月6日 下午1:30:46
-     * @return
      */
-    public  DistributedLock getDistributedLock(String lockKey){
-        DistributedLock distributedLock=new DistributedLockImpl(redissonClient,lockKey);
+    public DistributedLock getDistributedLock(String lockKey) {
+        DistributedLock distributedLock = new DistributedLockImpl(redissonClient, lockKey);
         return distributedLock;
     }
 
-    public  DistributedLock getDistributedLock(String lockKey,int expireMsecs){
-        DistributedLock distributedLock=new DistributedLockImpl(redissonClient,lockKey,expireMsecs);
+    public DistributedLock getDistributedLock(String lockKey, int expireMsecs) {
+        DistributedLock distributedLock = new DistributedLockImpl(redissonClient, lockKey, expireMsecs);
         return distributedLock;
     }
 
+    public DistributedLockUtil(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
 }
