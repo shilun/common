5d00f349c685fd7c38c9ae291eb2bc0b76115fc6
lunsh <lunshi@tt.com>
2018-12-02 17:28:43 +0800

    修改mongodb sort

--- src/main/java/com/common/redis/DistributedLockUtil.java
+++ src/main/java/com/common/redis/DistributedLockUtil.java
@@ -7,11 +7,10 @@ import javax.annotation.Resource;
 public class DistributedLockUtil{
     @Resource
      private RedissonClient redissonClient;
+
     /**
-     * 获取分布式锁
-     * 默认获取锁10s超时，锁过期时间60s
-     * @author yangwenkui
-     * @time 2016年5月6日 下午1:30:46
+     * 获取锁 默认 线程待等10 秒  锁 30秒
+     * @param lockKey
      * @return
      */
     public  DistributedLock getDistributedLock(String lockKey){
@@ -19,8 +18,26 @@ public class DistributedLockUtil{
         return distributedLock;
     }
 
-    public  DistributedLock getDistributedLock(String lockKey,int expireMsecs){
-        DistributedLock distributedLock=new DistributedLockImpl(redissonClient,lockKey,expireMsecs);
+    /**
+     * 获取锁  锁 30秒
+     * @param lockKey
+     * @param waitTime 线程等待时间 线程等持时间
+     * @return
+     */
+    public  DistributedLock getDistributedLock(String lockKey,int waitTime){
+        DistributedLock distributedLock=new DistributedLockImpl(redissonClient,lockKey,waitTime);
+        return distributedLock;
+    }
+
+    /**
+     * 获取锁
+     * @param key key
+     * @param waitTime 线程等待时间
+     * @param leaseTime 释放时间
+     * @return
+     */
+    public DistributedLock getDistributedLock(String key, int waitTime, int leaseTime){
+        DistributedLock distributedLock=new DistributedLockImpl(redissonClient,key,waitTime,leaseTime);
         return distributedLock;
     }
 
