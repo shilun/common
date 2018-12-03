ae9225ee9db2d562289cca71d44c692a6bfc9a6f
lunsh <lunshi@tt.com>
2018-12-03 10:56:26 +0800

    修改mongodb sort

--- src/main/java/com/common/config/MongoConfig.java
+++ src/main/java/com/common/config/MongoConfig.java
@@ -113,7 +113,7 @@ public class MongoConfig {
         if (StringUtils.isBlank(mongodbUrl)) {
             return null;
         }
-        com.mongodb.MongoClientURI url = new MongoClientURI(mongodbUrl, MongoClientOptions.builder().writeConcern(WriteConcern.ACKNOWLEDGED));
+        com.mongodb.MongoClientURI url = new MongoClientURI(mongodbUrl, MongoClientOptions.builder().writeConcern(WriteConcern.MAJORITY).readPreference( ReadPreference.secondary()));
         mongo = new MongoClient(url);
         return mongo;
     }
