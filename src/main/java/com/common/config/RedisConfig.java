fcc4e907b2fd6abb5fb9affd303a571e104d9999
lunsh <lunshi@tt.com>
2018-12-08 11:03:07 +0800

    修改 redis 锁

--- src/main/java/com/common/config/RedisConfig.java
+++ src/main/java/com/common/config/RedisConfig.java
@@ -83,39 +83,5 @@ public class RedisConfig extends CachingConfigurerSupport {
             return new DistributedLockUtil();
         }
     }
-    @ConditionalOnProperty(name="spring.redis.url")
-    class RedissonSingleConfig  {
-        @Value("${spring.redis.url}")
-        private String url;
-        @Bean
-        public RedissonClient getRedisson(){
-            Config config = new Config();
-            config.useSingleServer().setAddress(url);
-            return Redisson.create(config);
-        }
-        @Bean
-        public DistributedLockUtil distributedLockUtil() {
-            return new DistributedLockUtil(getRedisson());
-        }
-    }
-
-    @ConditionalOnProperty(name="spring.redis.cluster.nodes")
-    class RedissonClientClusterConfig  {
-        @Value("${spring.redis.cluster.nodes}")
-        private String url;
-        @Bean
-        public RedissonClient getRedisson(){
-            Config config = new Config();
-            String[] items = url.split(",");
-            for(String item:items){
-                config.useClusterServers().addNodeAddress("redis://"+ item);
-            }
-            return Redisson.create(config);
-        }
-        @Bean
-        public DistributedLockUtil distributedLockUtil() {
-            return new DistributedLockUtil(getRedisson());
-        }
-    }
 
 }
