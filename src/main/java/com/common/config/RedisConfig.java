ec007693c20cfff13b6ec6b3eb613a481fe8e2fd
lunsh <lunshi@tt.com>
2018-12-08 10:46:30 +0800

    修改mongodb sort

--- src/main/java/com/common/config/RedisConfig.java
+++ src/main/java/com/common/config/RedisConfig.java
@@ -4,6 +4,11 @@ import com.common.redis.DistributedLockUtil;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -44,6 +49,39 @@ public class RedisConfig extends CachingConfigurerSupport {
     public DistributedLockUtil distributedLockUtil() {
         return new DistributedLockUtil();
     }
+    @ConditionalOnProperty(name="spring.redis.url")
+    class RedissonSingleConfig  {
+        @Value("${spring.redis.url}")
+        private String url;
+        @Bean
+        public RedissonClient getRedisson(){
+            Config config = new Config();
+            config.useSingleServer().setAddress(url);
+            return Redisson.create(config);
+        }
+        @Bean
+        public DistributedLockUtil distributedLockUtil() {
+            return new DistributedLockUtil(getRedisson());
+        }
+    }
 
+    @ConditionalOnProperty(name="spring.redis.cluster.nodes")
+    class RedissonClientClusterConfig  {
+        @Value("${spring.redis.cluster.nodes}")
+        private String url;
+        @Bean
+        public RedissonClient getRedisson(){
+            Config config = new Config();
+            String[] items = url.split(",");
+            for(String item:items){
+                config.useClusterServers().addNodeAddress("redis://"+ item);
+            }
+            return Redisson.create(config);
+        }
+        @Bean
+        public DistributedLockUtil distributedLockUtil() {
+            return new DistributedLockUtil(getRedisson());
+        }
+    }
 
 }
