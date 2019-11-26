8a69ae6fd0e9c15a2b3a4720bdf05c07bab46840
lunsh <tt@tt.com>
2019-11-26 15:21:49 +0800

    增加mongodb 自定义主键值

---
+++ src/main/java/com/common/util/IDDto.java
@@ -0,0 +1,13 @@
+package com.common.util;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class IDDto implements Serializable {
+    /**
+     * 标识
+     */
+    private String id;
+}
