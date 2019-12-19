f1c9a4f5bb48e43b9e34fea9b50b00da6f9cdb66
lunsh <tt@tt.com>
2019-12-19 19:16:31 +0800

    添加资源实体

--- src/main/java/com/common/util/ResourceInfo.java
+++ src/main/java/com/common/util/ResourceInfo.java
@@ -1,5 +1,6 @@
 package com.common.util;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,13 +14,16 @@ public class ResourceInfo implements Serializable {
     /**
      * 排序
      */
+    @ApiModelProperty("排序")
     private Integer seq;
     /**
      * 类型 1 图片 2 语音 3 视频
      */
+    @ApiModelProperty("类型 1 图片 2 语音 3 视频")
     private Integer type;
     /**
-     * 资源url
+     * 资源url或内容
      */
+    @ApiModelProperty("资源url或内容")
     private String content;
 }
