45a20fd47bb336dfe496c4d9f505cfd385a12efe
lunsh <tt@tt.com>
2019-12-20 09:22:12 +0800

    添加资源实体

--- src/main/java/com/common/config/CommonConfig.java
+++ src/main/java/com/common/config/CommonConfig.java
@@ -6,6 +6,7 @@ import com.common.util.Money;
 import com.common.util.RPCResult;
 import com.common.util.StringUtils;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.extern.slf4j.Slf4j;
@@ -40,85 +41,82 @@ public class CommonConfig implements WebMvcConfigurer {
         return objectMapper;
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter() {
-            @Override
-            public void write(Object o, Type type, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-                super.writeInternal(o, outputMessage);
-            }
-            protected void writeInternal(Object e, HttpOutputMessage outputMessage) throws IOException {
-                HashMap map = new HashMap();
-                boolean seted = false;
-                if (e instanceof Map) {
-                    super.writeInternal(e, outputMessage);
-                } else {
-                    if (e instanceof RPCResult) {
-                        RPCResult result = (RPCResult) e;
-                        if (result.getSuccess()) {
-                            if (result.getTotalPage() != null && result.getTotalPage() > 0) {
-                                HashMap dataItem = new HashMap();
-                                dataItem.put("list", result.getData());
-                                dataItem.put("pageSize", Integer.valueOf(result.getPageSize()));
-                                dataItem.put("totalCount", Long.valueOf(result.getTotalCount()));
-                                dataItem.put("totalPage", Integer.valueOf(result.getTotalPage()));
-                                dataItem.put("pageIndex", Integer.valueOf(result.getPageIndex()));
-                                map.put("data", dataItem);
-                                map.put("success", result.getSuccess());
-                                super.writeInternal(map, outputMessage);
-                                return;
-                            }
-                            if (StringUtils.isNotBlank(result.getCode())) {
-                                map.put("code", result.getCode());
-                            }
-                            if (StringUtils.isNotBlank(result.getMessage())) {
-                                map.put("message", result.getMessage());
-                            }
-                            map.put("data", result.getData());
-                            map.put("success", result.getSuccess());
-                        } else {
-                            map.put("code", result.getCode());
-                            map.put("success", result.getSuccess());
-                            map.put("message", Boolean.valueOf(false));
-                        }
-                        super.writeInternal(map, outputMessage);
-                        return;
-                    }
-                    HashMap dataItem;
-                    if (e instanceof List) {
-                        seted = true;
-                        dataItem = new HashMap();
-                        dataItem.put("list", e);
-                        map.put("data", dataItem);
-                        map.put("success", Boolean.valueOf(true));
-                        super.writeInternal(map, outputMessage);
-                        return;
-                    }
-                    if (e instanceof Page) {
-                        seted = true;
-                        dataItem = new HashMap();
-                        Page page = (Page) e;
-                        dataItem.put("list", page.getContent());
-                        dataItem.put("pageSize", Integer.valueOf(page.getSize()));
-                        dataItem.put("totalCount", Long.valueOf(page.getTotalElements()));
-                        dataItem.put("totalPage", Integer.valueOf(page.getTotalPages()));
-                        dataItem.put("pageIndex", Integer.valueOf(page.getNumber()));
-                        map.put("data", dataItem);
-                        super.writeInternal(map, outputMessage);
-                        return;
-                    }
-                    if (!seted) {
-                        map.put("data", e);
-                    }
-                    map.put("success", Boolean.valueOf(true));
-                    super.writeInternal(map, outputMessage);
-                }
-            }
-        };
-        for (int i = 0; i < converters.size(); i++) {
-            if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
-                converters.set(i, fastConverter);
-            }
-        }
-    }
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        for (int i = 0; i < converters.size(); i++) {
+//            if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
+//                MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter() {
+//
+//                    @Override
+//                    protected void writeInternal(Object e, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+//                        Map<String, Object> map = new HashMap<>();
+//                        boolean seted = false;
+//                        if(e instanceof Map){
+//                            super.writeInternal(map,type, outputMessage);
+//                        }
+//                        if (e instanceof RPCResult) {
+//                            RPCResult result = (RPCResult) e;
+//                            if (result.getSuccess()) {
+//                                if (result.getTotalPage() != null && result.getTotalPage() > 0) {
+//                                    HashMap dataItem = new HashMap();
+//                                    dataItem.put("list", result.getData());
+//                                    dataItem.put("pageSize", Integer.valueOf(result.getPageSize()));
+//                                    dataItem.put("totalCount", Long.valueOf(result.getTotalCount()));
+//                                    dataItem.put("totalPage", Integer.valueOf(result.getTotalPage()));
+//                                    dataItem.put("pageIndex", Integer.valueOf(result.getPageIndex()));
+//                                    map.put("data", dataItem);
+//                                    map.put("success", result.getSuccess());
+//                                    super.writeInternal(map,type, outputMessage);
+//                                    return;
+//                                }
+//                                if (StringUtils.isNotBlank(result.getCode())) {
+//                                    map.put("code", result.getCode());
+//                                }
+//                                if (StringUtils.isNotBlank(result.getMessage())) {
+//                                    map.put("message", result.getMessage());
+//                                }
+//                                map.put("data", result.getData());
+//                                map.put("success", result.getSuccess());
+//                            } else {
+//                                map.put("code", result.getCode());
+//                                map.put("success", result.getSuccess());
+//                                map.put("message", Boolean.valueOf(false));
+//                            }
+//                            super.writeInternal(map,type, outputMessage);
+//                            return;
+//                        }
+//                        HashMap dataItem;
+//                        if (e instanceof List) {
+//                            seted = true;
+//                            dataItem = new HashMap();
+//                            dataItem.put("list", e);
+//                            map.put("data", dataItem);
+//                            map.put("success", Boolean.valueOf(true));
+//                            super.writeInternal(map,type, outputMessage);
+//                            return;
+//                        }
+//                        if (e instanceof Page) {
+//                            seted = true;
+//                            dataItem = new HashMap();
+//                            Page page = (Page) e;
+//                            dataItem.put("list", page.getContent());
+//                            dataItem.put("pageSize", Integer.valueOf(page.getSize()));
+//                            dataItem.put("totalCount", Long.valueOf(page.getTotalElements()));
+//                            dataItem.put("totalPage", Integer.valueOf(page.getTotalPages()));
+//                            dataItem.put("pageIndex", Integer.valueOf(page.getNumber()));
+//                            map.put("data", dataItem);
+//                            super.writeInternal(map,type, outputMessage);
+//                            return;
+//                        }
+//                        if (!seted) {
+//                            map.put("data", e);
+//                        }
+//                        map.put("success", Boolean.valueOf(true));
+//                        super.writeInternal(e, type, outputMessage);
+//                    }
+//                };
+//                converters.set(i, converter);
+//            }
+//        }
+//    }
 }
