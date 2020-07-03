56b43f25d6de94697690bedfaa38558683455e5a
mac <xxx@qq.copm>
2020-07-03 12:15:21 +0800

    修改

--- src/main/java/com/common/config/MongoConfig.java
+++ src/main/java/com/common/config/MongoConfig.java
@@ -16,99 +16,52 @@
 
 package com.common.config;
 
-import com.common.mongo.LongToMoneyConvert;
-import com.common.mongo.MoneyToLongConvert;
 import com.common.mongo.SaveMongoEventListener;
 import com.common.util.StringUtils;
 import com.mongodb.ReadPreference;
 import com.mongodb.WriteConcern;
-import org.bson.conversions.Bson;
-import org.springframework.beans.BeansException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.*;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.util.TypeInformation;
-
-import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for Mongo.
  */
 @Configuration
-@Conditional(MongodbCondition.class)
-public class MongoConfig implements ApplicationContextAware, ResourceLoaderAware {
-
-    private ApplicationContext applicationContext;
-    private ResourceLoader resourceLoader;
+public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Value("${spring.data.mongodb.uri}")
     private String mongodbUrl;
 
-    @ConditionalOnProperty(name = "app.db.transaction")
-    class MongoTransactionConfig {
-
-        @Value("${app.db.transaction}")
-        private Boolean transaction;
-
-        @Bean
-        public TransBean getTransBean() {
-            TransBean transBean = new TransBean();
-            transBean.setTransaction(transaction);
-            return transBean;
-        }
-
-        @Bean
-        public MongoTransactionManager transactionManager() {
-            return new MongoTransactionManager(dbFactory);
-        }
-    }
-
-    @Resource
-    private MongoDatabaseFactory dbFactory;
-
     @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    protected boolean autoIndexCreation() {
+        return true;
     }
 
     @Bean("primary")
-    public MongoTemplate mongoPrimaryTemplate() throws Exception {
+    public MongoTemplate mongoPrimaryTemplate(MongoDatabaseFactory databaseFactory, MappingMongoConverter converter) throws Exception {
         if (StringUtils.isBlank(mongodbUrl)) {
             throw new Exception("mongodb load error url" + mongodbUrl);
         }
-        MongoTemplate mongoTemplate = new MongoTemplate(dbFactory);
+        MongoTemplate mongoTemplate = new MongoTemplate(databaseFactory, converter);
         mongoTemplate.setReadPreference(ReadPreference.primary());
         mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
         return mongoTemplate;
     }
 
     @Bean("secondary")
-    public MongoTemplate mongoSecondaryTemplate() throws Exception {
+    public MongoTemplate mongoSecondaryTemplate(MongoDatabaseFactory databaseFactory, MappingMongoConverter converter) throws Exception {
         if (StringUtils.isBlank(mongodbUrl)) {
             throw new Exception("mongodb load error url" + mongodbUrl);
         }
-        MongoTemplate mongoTemplate = new MongoTemplate(dbFactory);
+        MongoTemplate mongoTemplate = new MongoTemplate(databaseFactory, converter);
         mongoTemplate.setReadPreference(ReadPreference.secondary());
         mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
         return mongoTemplate;
@@ -120,6 +73,15 @@ public class MongoConfig implements ApplicationContextAware, ResourceLoaderAware
     }
 
 
+    @Override
+    protected String getDatabaseName() {
+        return mongoClient().listDatabaseNames().first();
+    }
+
+    public @Bean
+    MongoClient mongoClient() {
+        return MongoClients.create(mongodbUrl);
+    }
 }
 
 
