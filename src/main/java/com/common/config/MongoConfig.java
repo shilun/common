95a38cbe0960972fe39acd9dde2c85f2d61927dd
mac <xxx@qq.copm>
2020-08-07 12:13:44 +0800

    修改

--- src/main/java/com/common/config/MongoConfig.java
+++ src/main/java/com/common/config/MongoConfig.java
@@ -16,72 +16,116 @@
 
 package com.common.config;
 
-import com.common.mongo.SaveMongoEventListener;
+import com.common.mongo.*;
 import com.common.util.StringUtils;
 import com.mongodb.ReadPreference;
 import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.bson.conversions.Bson;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.util.TypeInformation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * {@link EnableAutoConfiguration Auto-configuration} for Mongo.
  */
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+@Conditional(MongodbCondition.class)
+public class MongoConfig {
+
 
     @Value("${spring.data.mongodb.uri}")
-    private String mongodbUrl;
+    private String mongodbUri;
 
-    @Override
-    protected boolean autoIndexCreation() {
-        return true;
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(){
+        return new SimpleMongoClientDatabaseFactory(mongodbUri);
     }
 
     @Bean("primary")
     public MongoTemplate mongoPrimaryTemplate(MongoDatabaseFactory mongoDatabaseFactory, MappingMongoConverter converter) throws Exception {
-        if (StringUtils.isBlank(mongodbUrl)) {
-            throw new Exception("mongodb load error url" + mongodbUrl);
-        }
+
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDatabaseFactory, converter);
         mongoTemplate.setReadPreference(ReadPreference.primary());
         mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
         return mongoTemplate;
     }
-    @Bean
-    public MongoDatabaseFactory mongoDatabaseFactory(){
-        return new SimpleMongoClientDatabaseFactory(mongodbUrl);
-    }
 
     @Bean("secondary")
     public MongoTemplate mongoSecondaryTemplate(MongoDatabaseFactory mongoDatabaseFactory, MappingMongoConverter converter) throws Exception {
-        if (StringUtils.isBlank(mongodbUrl)) {
-            throw new Exception("mongodb load error url" + mongodbUrl);
-        }
+
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDatabaseFactory, converter);
         mongoTemplate.setReadPreference(ReadPreference.secondary());
         mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
         return mongoTemplate;
     }
+    @Bean
+    public MongoCustomConversions customConversions() {
+        List<Converter> list = new ArrayList();
+        list.add(new MoneyToLongConvert());
+        list.add(new LongToMoneyConvert());
+        return new MongoCustomConversions(list);
+    }
+    @Bean
+    @ConditionalOnMissingBean
+    public MongoMappingContext mongoMappingContext(BeanFactory beanFactory, ApplicationContext applicationContext) throws ClassNotFoundException {
+        MongoMappingContext context = new MongoMappingContext() {
+            @Override
+            public Collection<BasicMongoPersistentEntity<?>> getPersistentEntities() {
+                return super.getPersistentEntities();
+            }
+        };
+
+        return context;
+    }
+
 
     @Bean
-    public SaveMongoEventListener mongoEventListener() {
-        return new SaveMongoEventListener();
-    }
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory, MongoMappingContext mongoMappingContext) {
+        mongoMappingContext.setAutoIndexCreation(true);
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext){
+        };
+
+        DefaultMongoTypeMapper typeMapper = new DefaultMongoTypeMapper(null){
+            @Override
+            public <T> TypeInformation<? extends T> readType(Bson source, TypeInformation<T> basicType) {
+                return super.readType(source, basicType);
+            }
+
+            @Override
+            public TypeInformation<?> readType(Bson source) {
+                return super.readType(source);
+            }
+        };
+
+        mappingConverter.setTypeMapper(typeMapper);//去掉默认mapper添加的_class
+        mappingConverter.setCustomConversions(customConversions());//添加自定义的转换器
 
 
-    @Override
-    protected String getDatabaseName() {
-        return mongodbUrl.substring(mongodbUrl.lastIndexOf("/")+1);
+        return mappingConverter;
     }
+
 
 }
 
